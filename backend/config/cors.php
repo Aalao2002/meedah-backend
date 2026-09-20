a80bf58000c5628f6d8d2144bcf77d80aa6adf0e
alao billz <93359182+Aalao2002@users.noreply.github.com>
2026-09-20 19:23:23 +0900

    Update cors.php
    Fix cors url for render

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
  return [
     'paths' => ['api/*', 'sanctum/crsf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://meedahbakes.vercel.app'],
+    'allowed_origins' => ['https://meedahbakes.netlify.app'],
     'allowed_origin_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
