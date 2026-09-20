53e934a286f7f31ed0ed091850fb36c41773d1a4
alao billz <aalao2002@gmail.com>
2026-09-20 05:28:30 -0700

    wire cors allowed origins to FRONTENDL env var

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000',
-        env('FRONTEND_URL')
+        'https://meedahbakes.netlify.app'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
